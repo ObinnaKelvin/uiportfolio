d812fcfd35764e28a81ece7b81a77e26a3dfa0ed
Obinna Okere <obinna.okere@lagoonhospitals.com>
2022-06-09 19:05:50 +0100

    Updating Explore page

--- src/components/explore/explore.jsx
+++ src/components/explore/explore.jsx
@@ -1,13 +1,19 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from "../../context.js";
 import "./explore.css";
+import construction from "../../img/construction.png"
 
 const Explore = () => {
 		// const theme = useContext(ThemeContext);
   // 		const darkMode = theme.state.darkMode;
 	return (
 		//<div style={{backgroundColor: darkMode ? "#222" : "white", color: darkMode && "white"}}>
-		<div>
+		<div className="exp">
+
+			<img className="exp-image"src={construction}/>
+			<p>
+				<h3 className="exp-caption">Sorry, this page will be back up soon.</h3>
+			</p>
 			
 		</div>
 	)
